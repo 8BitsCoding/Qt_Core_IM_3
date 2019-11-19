6ae833cf991700cd5767fbfd81481bd5bc8c0805
taehyung77@naver.com <taehyung77@naver.com>
2019-11-19 16:07:48 +0900

    comit

--- Qt_Core_IM_3/main.cpp
+++ Qt_Core_IM_3/main.cpp
@@ -1,8 +1,36 @@
 #include <QtCore/QCoreApplication>
 
+// The QScopedPointer class stores a pointer to a dynamically allocated object,
+// and deletes it upon destruction!!!
+
+#include <qdebug.h>
+#include <qscopedpointer.h>
+
+#include "test.h"
+
+void useit(test* obj) {
+	if (!obj) return;
+
+	qInfo() << "Using" << obj;
+}
+
+void doStuff() {
+	//test* t = new test();			// dangling(달랑달랑) pointer!!!
+	QScopedPointer<test> mypointer(new test());
+
+	//이렇게 포인터를 넣을 수 있다.
+	useit(mypointer.data());		
+
+	// pointer is automatically deleted!!
+}
+
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+
+	for (int i = 0; i < 100; i++) {
+		doStuff();
+	}
 
 	return a.exec();
 }
---
+++ Qt_Core_IM_3/test.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <qobject.h>
+#include <qdebug.h>
+
+class test : public QObject
+{
+	Q_OBJECT
+public:
+	explicit test(QObject* parent = nullptr);
+	~test();
+
+signals:
+
+public slots:
+};
+
